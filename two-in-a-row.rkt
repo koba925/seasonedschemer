9eeaf5e14df23df7ab5173d4362d73e278994f94
Takahiro Kobayashi <koba925@gmail.com>
2015-07-17 13:08:39 +0900

    two-in-a-row(自分で書いた版)を追加

---
+++ two-in-a-row.rkt
@@ -0,0 +1,24 @@
+#lang racket/base
+
+(require rackunit)
+(require "util.rkt")
+
+(define two-in-a-row?
+  (lambda (lat)
+    (cond ((null? lat) #f)
+          ((is-first? (car lat) (cdr lat)) #t)
+          (else (two-in-a-row? (cdr lat))))))
+
+(define is-first?
+  (lambda (a lat)
+    (cond ((null? lat) #f)
+          ((eq? (car lat) a) #t)
+          (else #f))))
+
+(check-false (two-in-a-row? '()))
+(check-false (two-in-a-row? '(a)))
+(check-true (two-in-a-row? '(a a)))
+(check-true (two-in-a-row? '(a a b)))
+(check-true (two-in-a-row? '(a b b)))
+(check-false (two-in-a-row? '(a b c)))
+
