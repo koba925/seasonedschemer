142d5b59208c1f4dea87ad7e3ce9de3f7bd41520
Takahiro Kobayashi <koba925@gmail.com>
2015-07-27 21:47:47 +0900

    multirember(手習い版)を追加

--- chap12.rkt
+++ chap12.rkt
@@ -3,3 +3,14 @@
 (require rackunit)
 (require "util.rkt")
 
+(define multirember
+  (lambda (a lat)
+    (cond ((null? lat) (quote ()))
+          ((eq? (car lat) a) (multirember a (cdr lat)))
+          (else (cons (car lat) (multirember a (cdr lat)))))))
+
+(check-equal? (multirember 'a '()) '())
+(check-equal? (multirember 'a '(a b)) '(b))
+(check-equal? (multirember 'a '(b c)) '(b c))
+(check-equal? (multirember 'a '(a b a)) '(b))
+
