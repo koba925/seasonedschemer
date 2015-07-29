5bd46c69aad9f4f9ec3ec0d366619134bc6b8daf
Takahiro Kobayashi <koba925@gmail.com>
2015-07-29 21:39:22 +0900

    multirember各種を追加

--- chap12.rkt
+++ chap12.rkt
@@ -3,7 +3,7 @@
 (require rackunit)
 (require "util.rkt")
 
-(define multirember
+(define multirember-y
   (lambda (a lat)
     ((Y
       (lambda (mr)
@@ -13,8 +13,51 @@
                 (else (cons (car lat) (mr (cdr lat))))))))
      lat)))
 
-(check-equal? (multirember 'a '()) '())
-(check-equal? (multirember 'a '(a b)) '(b))
-(check-equal? (multirember 'a '(b c)) '(b c))
-(check-equal? (multirember 'a '(a b a)) '(b))
+(check-equal? (multirember-y 'a '()) '())
+(check-equal? (multirember-y 'a '(a b)) '(b))
+(check-equal? (multirember-y 'a '(b c)) '(b c))
+(check-equal? (multirember-y 'a '(a b a)) '(b))
 
+(define multirember-r
+  (lambda (a lat)
+    ((letrec
+         ((mr (lambda (lat)
+                (cond ((null? lat) (quote ()))
+                      ((eq? (car lat) a) (mr (cdr lat)))
+                      (else (cons (car lat) (mr (cdr lat))))))))
+       mr)
+     lat)))
+
+(check-equal? (multirember-r 'a '()) '())
+(check-equal? (multirember-r 'a '(a b)) '(b))
+(check-equal? (multirember-r 'a '(b c)) '(b c))
+(check-equal? (multirember-r 'a '(a b a)) '(b))
+
+
+(define multirember-d
+  (lambda (a lat)
+    (define mr
+      (lambda (lat)
+        (cond ((null? lat) (quote ()))
+              ((eq? a (car lat)) (mr (cdr lat)))
+              (else (cons (car lat) (mr (cdr lat)))))))
+    (mr lat)))
+
+(check-equal? (multirember-d 'a '()) '())
+(check-equal? (multirember-d 'a '(a b)) '(b))
+(check-equal? (multirember-d 'a '(b c)) '(b c))
+(check-equal? (multirember-d 'a '(a b a)) '(b))
+
+(define multirember-r2
+  (lambda (a lat)
+    (letrec
+        ((mr (lambda (lat)
+               (cond ((null? lat) (quote ()))
+                     ((eq? (car lat) a) (mr (cdr lat)))
+                     (else (cons (car lat) (mr (cdr lat))))))))
+      (mr lat))))
+
+(check-equal? (multirember-r2 'a '()) '())
+(check-equal? (multirember-r2 'a '(a b)) '(b))
+(check-equal? (multirember-r2 'a '(b c)) '(b c))
+(check-equal? (multirember-r2 'a '(a b a)) '(b))
