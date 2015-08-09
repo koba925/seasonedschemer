f9a787f561b232aa29f96cf9325bbc18031fee13
Takahiro Kobayashi <koba925@gmail.com>
2015-08-10 08:56:41 +0900

    intersectall let/cc版

--- chap13.rkt
+++ chap13.rkt
@@ -21,13 +21,16 @@
 
 (define intersectall
   (lambda (lset)
-    (letrec
-        ((A (lambda (lset)
-              (cond ((null? (cdr lset)) (car lset))
-                    (else (intersect (car lset)
-                                     (A (cdr lset))))))))
-      (cond ((null? lset) (quote ()))
-            (else (A lset))))))
+    (let/cc hop
+      (letrec
+          ((A (lambda (lset)
+                (cond ((null? (car lset))
+                       (hop (quote ())))
+                      ((null? (cdr lset)) (car lset))
+                      (else (intersect (car lset)
+                                       (A (cdr lset))))))))
+        (cond ((null? lset) (quote()))
+              (else (A lset)))))))
 
 (check-equal? (intersectall '()) '())
 (check-equal? (intersectall '(())) '())
@@ -37,3 +40,14 @@
 (check-equal? (intersectall '((a) (a) (a))) '(a))
 (check-equal? (intersectall '((a) (a) (b))) '())
 (check-equal? (intersectall '((a b c) (c a d) (b e c))) '(c))
+(check-equal? (intersectall '((a b c) () (b e c))) '())
+
+; (intersectall-c '((a b c) () (b e c)))
+; (let/cc hop (A '((a b c) () (b e c))))
+; (let/cc hop (intersect '(a b c) (A '(() (b e c)))))
+; (let/cc hop (intersect '(a b c) (hop (quote ()))))
+; (let/cc hop (quote ()))
+; (quote ())
+
+
+
