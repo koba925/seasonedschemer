0fc8d8908bbb0bcfae72f6001640bc9e48c71ad6
Takahiro Kobayashi <koba925@gmail.com>
2015-12-01 17:59:05 +0900

    deepM内にdeepの定義を入れた

--- chap17.rkt
+++ chap17.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(require "util.rkt")
+
 (define deep
   (lambda (m)
     (if (zero? m)
@@ -8,3 +10,31 @@
 
 (deep 0)
 (deep 3)
+
+(define find
+  (lambda (n Ns Rs)
+    (cond ((null? Ns) #f)
+          ((eq? (car Ns) n) (car Rs))
+          (else (find n (cdr Ns) (cdr Rs))))))
+
+(find 3 '(1 2 3 4) '(a b c d))
+(find 0 '(1 2 3 4) '(a b c d))
+
+(define deepM
+  (let ((Rs (quote ()))
+        (Ns (quote ())))
+    (letrec ((D (lambda (m)
+                  (if (zero? m)
+                      (quote pizza)
+                      (cons (D (sub1 m)) (quote ()))))))
+      (lambda (n)
+        (let ((exists (find n Ns Rs)))
+          (if (atom? exists)
+              (let ((result (D n)))
+                (set! Rs (cons result Rs))
+                (set! Ns (cons n Ns))
+                result)
+              exists))))))
+
+(deepM 0)
+(deepM 3)
